73ad15cc1d6cb55606f9b54276012b05b20b24ca
vandevee3 <garindrawardhana@gmail.com>
2026-03-19 17:27:31 +0900

    fixing processing directory path

--- src/processing.py
+++ src/processing.py
@@ -1,24 +1,23 @@
 from .utils import deserialize_data, create_onehot_encoder, ohe_transform
 import yaml
+from pathlib import Path
 
 def ohe_transform_full(df):
-    with open('../config/path.yaml', 'r') as f:
-        path = yaml.safe_load(f)
+    BASE_DIR = Path(__file__).resolve().parent.parent  
+    # CONFIG_PATH = BASE_DIR / "config" / "path.yaml"
 
-    ohe_home_ownership_path = path['data_path']['models']['ohe_home_ownership']
-    ohe_loan_intent_path = path['data_path']['models']['ohe_loan_intent']
-    ohe_loan_grade_path = path['data_path']['models']['ohe_loan_grade']
-    ohe_default_on_file_path = path['data_path']['models']['ohe_default_on_file']
+    # with open(CONFIG_PATH, 'r') as f:
+    #     path = yaml.safe_load(f)
 
-    OHE_HOME_OWNERSHIP_PATH = ohe_home_ownership_path
-    OHE_LOAN_INTENT_PATH = ohe_loan_intent_path
-    OHE_LOAN_GRADE_PATH = ohe_loan_grade_path
-    OHE_DEFAULT_FILE_ON_PATH = ohe_default_on_file_path
+    OHE_HOME_OWNERSHIP_PATH  = BASE_DIR / "models" / "ohe_home_ownership.pkl"
+    OHE_DEFAULT_ON_FILE_PATH = BASE_DIR / "models" / "ohe_default_on_file.pkl"
+    OHE_LOAN_GRADE_PATH      = BASE_DIR / "models" / "ohe_loan_grade.pkl"
+    OHE_LOAN_INTENT_PATH     = BASE_DIR / "models" / "ohe_loan_intent.pkl"
 
     ohe_transform_ownership = deserialize_data(OHE_HOME_OWNERSHIP_PATH)
     ohe_loan_intent = deserialize_data(OHE_LOAN_INTENT_PATH)
     ohe_loan_grade = deserialize_data(OHE_LOAN_GRADE_PATH)
-    ohe_default_on_file = deserialize_data(OHE_DEFAULT_FILE_ON_PATH)
+    ohe_default_on_file = deserialize_data(OHE_DEFAULT_ON_FILE_PATH)
 
     df = ohe_transform(df, 'person_home_ownership', 'home_ownership', ohe_transform_ownership)
     df = ohe_transform(df, 'loan_intent', 'loan_intent', ohe_loan_intent)
